c8b90e57e3d66db9e074a0dc2fee0ec44b696f16
Bilal Ahmed <bilal434ahmed@gmail.com>
2016-12-03 11:24:05 +0500

    Update main.c

--- main.c
+++ main.c
@@ -3,78 +3,21 @@
 #include "tm4c123gh6pm.h"
 #include "Header.h"
 
-int Adc_Value;
-int User_Value = 25;
+int Adc_Value, Pre_State, Current_State = 0, User_Value = 25;
 
 int main(void){
-		PLL_Init();
-		SysTick_Init();
-		PortF_Init();
-		PortF_Interrupt();
-		LCD_Init();
-		ADC_Init();
-		
-		SysTick_Delay1ms(10);
-		LCD_DisplayPosition(LINE1,1);
-		printf("MICROPROCESSOR");
-		SysTick_Delay1ms(1000);
-		LCD_DisplayPosition(LINE2,4);
-		printf("PROJECT");
-		SysTick_Delay1ms(2000);
-		LCD_Clear();
-		
-		SysTick_Delay1ms(10);
-		LCD_DisplayPosition(LINE1,2);
-		printf("Temperature");
-		SysTick_Delay1ms(1000);
-		LCD_DisplayPosition(LINE2,1);
-		printf("Ctrl Fan/Device");
-		SysTick_Delay1ms(2000);
-		LCD_Clear();
-	
-		SysTick_Delay1ms(10);
-		LCD_DisplayPosition(LINE1,0);
-		printf("By: BILAL AHMED");
-		SysTick_Delay1ms(1000);
-		LCD_DisplayPosition(LINE2,2);
-		printf("2014-EE-366");
-		SysTick_Delay1ms(2000);
-		LCD_Clear();
-		
-		SysTick_Delay1ms(10);
-		LCD_DisplayPosition(LINE1,2);
-		printf("MOIN AKHTAR");
-		SysTick_Delay1ms(1000);
-		LCD_DisplayPosition(LINE2,3);
-		printf("UMER ALYAN");
-		SysTick_Delay1ms(1000);
-		LCD_Clear();
-		
-		SysTick_Delay1ms(10);
-		LCD_DisplayPosition(LINE1,0);
-		printf("To: Sir ");
-		SysTick_Delay1ms(1000);
-		LCD_DisplayPosition(LINE2,4);
-		printf("FAHID IJAZ");
-		SysTick_Delay1ms(2000);
-		LCD_Clear();
-		
+	Init_Intro();
+	Adc_Value = ADC_Value();
+	Pre_State = GetState(Adc_Value, User_Value);
+	TestS(User_Value, Adc_Value);
 			
-while(1){
-			Adc_Value = ADC_Data();
-			Adc_Value /= 10;
-	
-			SysTick_Delay1ms(10);
-			LCD_DisplayPosition(LINE1,0); 
-			printf("NOW TEMP is %d C",Adc_Value);
-			SysTick_Delay1ms(2000);
-			LCD_DisplayPosition(LINE2,1); 
-			printf("USER SET %d C",User_Value);
-			SysTick_Delay1ms(2000);
-			LCD_Clear();
-			SysTick_Delay1ms(10);
-			Test(User_Value, Adc_Value);
-    }
+	while(1){
+		Adc_Value = ADC_Value();
+		Current_State = Test(User_Value, Adc_Value);
+		Repeat_Display();
+		Drive(Current_State,Pre_State);
+		Pre_State = Current_State;
+	}
 }
 
 void GPIOPortF_Handler(void){
@@ -83,15 +26,25 @@ void GPIOPortF_Handler(void){
 		LCD_Clear();
 		LCD_DisplayPosition(LINE1, 1);
 		printf("USER SET ++ %d C", User_Value);
-		SysTick_Delay1ms(500);
+		Delay1ms(500);
 		GPIO_PORTF_ICR_R = 0x10;
 	}
 	if ((GPIO_PORTF_DATA_R & 0x01) == 0){
 		User_Value--;
 		LCD_Clear(); LCD_DisplayPosition(LINE1, 1);
-		printf("USER SET ++ %d C", User_Value);
-		SysTick_Delay1ms(500);
+		printf("USER SET -- %d C", User_Value);
+		Delay1ms(500);
 		GPIO_PORTF_ICR_R = 0x01;
 	}
 }
-
+void Repeat_Display(void){
+	Delay1ms(10);
+	LCD_DisplayPosition(LINE1,0); 
+	printf("NOW TEMP is %d C",Adc_Value);
+	Delay1ms(10);
+	LCD_DisplayPosition(LINE2,1); 
+	printf("USER SET %d C",User_Value);
+	Delay1ms(3000);
+	LCD_Clear();
+	Delay1ms(3);
+}

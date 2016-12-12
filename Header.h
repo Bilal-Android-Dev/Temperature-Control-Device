24055e6edc2e544b632a300a1ff636d52ddd99e3
Bilal Ahmed <bilal434ahmed@gmail.com>
2016-12-12 20:06:21 +0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -7,16 +7,14 @@
 //LCD
 void LCD_Init(void);
 void LCD_Clear(void);
-void LCD_DisplayString(unsigned char *str);
 void LCD_DisplayChar(unsigned char CHAR);
-void LCD_DisplayDec(unsigned int number);
 void LCD_DisplayPosition(unsigned char Line, unsigned int digit);
 
 //PLL
 void PLL_Init(void);
 
 //SYSTICK
-void SysTick_Init(void);
+voidSysTick_Init(void);
 void SysTick_Delay(unsigned long delay);
 void SysTick_Delay1us(unsigned long delay);
 void Delay1ms(unsigned long delay);

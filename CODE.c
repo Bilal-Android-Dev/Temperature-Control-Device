f3dfb12899aae1376622067e2e3093c9c1863664
Bilal Ahmed <bilal434ahmed@gmail.com>
2016-12-12 20:09:23 +0500

    Update CODE.c

--- CODE.c
+++ CODE.c
@@ -162,7 +162,7 @@ unsigned int ADC_Data(void){
 int ADC_Value(void){
 	int result, div = 0;
 	result = ADC_Data();
-	div = (result * 10000) / 140;
+	div = (result * 10000) / 140000;
 	return div;
 }
 //ADC END
